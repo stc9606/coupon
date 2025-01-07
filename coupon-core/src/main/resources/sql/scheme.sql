be1ad3c79a79fd01d5623ff7cb82e87ade67b698
scnoh <stc9606@naver.com>
2025-01-07 21:51:00 +0900

    feat. 쿠폰 발급 테이블 스키마 작성

---
+++ coupon-core/src/main/resources/sql/scheme.sql
@@ -0,0 +1,32 @@
+create table `coupon`.`coupon`
+(
+    `id`                        bigint(20) not null auto_increment,
+    `title`                     varchar(255) not null comment '쿠폰명',
+    `coupon_type`               varchar(255) not null comment '쿠폰 타입 (선착순 쿠폰, ..)',
+    `total_quantity`            int null comment '쿠폰 발급 최대 수량',
+    `issued_quantity`           int not null '발급된 쿠폰 수량',
+    `discount_amount`           int not null '할인 금액',
+    `min_available_amount`      int not null '최소 사용 금액',
+    `date_issue_start`          datetime(6) not null comment '발급 시작 일시',
+    `date_issue_end`            datetime(6) not null comment '발급 종료 일시',
+    `date_created`              datetime(6) not null comment '생성 일시',
+    `date_updated`              datetime(6) not null comment '수정 일시',
+    primary key (`id`)
+) engine = InnoDB
+    default charset = utf8mb4
+        comment '쿠폰 정책';
+
+create table `coupon`.`coupon_issues`
+(
+    `id`                        bigint(20) not null auto_increment,
+    `coupon_id`                 bigint(20) not null comment '쿠폰 ID',
+    `use_id`                    bigint(20) not null comment '유저 ID',
+    `title`                     varchar(255) not null comment '쿠폰명',
+    `date_issue_start`          datetime(6) not null comment '발급 시작 일시',
+    `date_issue_end`            datetime(6) not null comment '발급 종료 일시',
+    `date_created`              datetime(6) not null comment '생성 일시',
+    `date_updated`              datetime(6) not null comment '수정 일시',
+    primary key (`id`)
+) engine = InnoDB
+    default charset = utf8mb4
+        comment '쿠폰 발급 내역';
