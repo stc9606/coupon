3a4454dec1bfb005a9dd8c7fd3e9aedbdeaca132
scnoh <stc9606@naver.com>
2025-01-08 22:00:53 +0900

    modify. 쿠폰 발급 테이블 스키마 수정

--- coupon-core/src/main/resources/sql/scheme.sql
+++ coupon-core/src/main/resources/sql/scheme.sql
@@ -1,4 +1,4 @@
-create table `coupon`.`coupon`
+create table `coupon`.`coupons`
 (
     `id`                        bigint(20) not null auto_increment,
     `title`                     varchar(255) not null comment '쿠폰명',
@@ -22,8 +22,8 @@ create table `coupon`.`coupon_issues`
     `coupon_id`                 bigint(20) not null comment '쿠폰 ID',
     `use_id`                    bigint(20) not null comment '유저 ID',
     `title`                     varchar(255) not null comment '쿠폰명',
-    `date_issue_start`          datetime(6) not null comment '발급 시작 일시',
-    `date_issue_end`            datetime(6) not null comment '발급 종료 일시',
+    `date_issued`               datetime(6) not null comment '발급 일시',
+    `date_used`                 datetime(6) null comment '사용 일시',
     `date_created`              datetime(6) not null comment '생성 일시',
     `date_updated`              datetime(6) not null comment '수정 일시',
     primary key (`id`)
